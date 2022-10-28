0a01548f8b150b4f327f9554446cf4f8aa123280
SultanKirigha <sultan.kirigha@student.moringaschool.com>
2022-10-28 17:09:42 +0300

    Updates

---
+++ precache-manifest.d838fce17d113ba21acbcac85366b2e1.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "4366105650572fedb405",
+    "url": "/My-Personal-Portfolio/static/css/main.f43ca146.chunk.css"
+  },
+  {
+    "revision": "4366105650572fedb405",
+    "url": "/My-Personal-Portfolio/static/js/main.43661056.chunk.js"
+  },
+  {
+    "revision": "ef9691e0a9d3c4a77e41",
+    "url": "/My-Personal-Portfolio/static/js/1.ef9691e0.chunk.js"
+  },
+  {
+    "revision": "dfc3c56f03e98117d275",
+    "url": "/My-Personal-Portfolio/static/css/2.b198da76.chunk.css"
+  },
+  {
+    "revision": "dfc3c56f03e98117d275",
+    "url": "/My-Personal-Portfolio/static/js/2.dfc3c56f.chunk.js"
+  },
+  {
+    "revision": "35b76e4560f7cc90b053",
+    "url": "/My-Personal-Portfolio/static/js/runtime~main.35b76e45.js"
+  },
+  {
+    "revision": "05ba3eb86ea469f95858762e47d2de5d",
+    "url": "/My-Personal-Portfolio/static/media/Logo.05ba3eb8.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/My-Personal-Portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/My-Personal-Portfolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/My-Personal-Portfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/My-Personal-Portfolio/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "d2070d4b04662b662514a5951877baab",
+    "url": "/My-Personal-Portfolio/static/media/meter1.d2070d4b.svg"
+  },
+  {
+    "revision": "c1b4e9e8bbdc02a21e2e5e0f0fb1904e",
+    "url": "/My-Personal-Portfolio/static/media/meter2.c1b4e9e8.svg"
+  },
+  {
+    "revision": "796b116d3d0da2a4af4cac731407a884",
+    "url": "/My-Personal-Portfolio/static/media/meter3.796b116d.svg"
+  },
+  {
+    "revision": "e2b4980aa25f719d54c772b6302ac379",
+    "url": "/My-Personal-Portfolio/static/media/meter4.e2b4980a.svg"
+  },
+  {
+    "revision": "2fd5055135320513df4314a73ea2f68a",
+    "url": "/My-Personal-Portfolio/static/media/Music App Daily UI 10.2fd50551.png"
+  },
+  {
+    "revision": "03fb8abc597bab78efb5c1481aaa0887",
+    "url": "/My-Personal-Portfolio/static/media/Advertising Agence Landing Page.03fb8abc.png"
+  },
+  {
+    "revision": "203266f7d7dfa1716cdf907cb1df4fc4",
+    "url": "/My-Personal-Portfolio/static/media/Frame.203266f7.png"
+  },
+  {
+    "revision": "c4e6d3fe6f866310376dc3ccfbdf480f",
+    "url": "/My-Personal-Portfolio/static/media/Landing Page.c4e6d3fe.png"
+  },
+  {
+    "revision": "56a16dd31083a9eeaa381a83bcf8f3a8",
+    "url": "/My-Personal-Portfolio/static/media/logo1.56a16dd3.png"
+  },
+  {
+    "revision": "c97d7304e8830fe9dd4099738ae76603",
+    "url": "/My-Personal-Portfolio/static/media/Movie.c97d7304.png"
+  },
+  {
+    "revision": "b86189d66c52bf183a09f66aa1a82f9a",
+    "url": "/My-Personal-Portfolio/static/media/Food App.b86189d6.png"
+  },
+  {
+    "revision": "88d371c42979f0fbd80989cc05304e5c",
+    "url": "/My-Personal-Portfolio/static/media/Alert.88d371c4.png"
+  },
+  {
+    "revision": "7f6117c247e3a002540d7031b66e27f7",
+    "url": "/My-Personal-Portfolio/static/media/Prototype.7f6117c2.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/My-Personal-Portfolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "24866c0201527c13b1550845e02d11d6",
+    "url": "/My-Personal-Portfolio/static/media/logo.24866c02.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/My-Personal-Portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/My-Personal-Portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/My-Personal-Portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/My-Personal-Portfolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/My-Personal-Portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/My-Personal-Portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/My-Personal-Portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/My-Personal-Portfolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "34478024f4528a90964143ed528e3d47",
+    "url": "/My-Personal-Portfolio/index.html"
+  }
+];
